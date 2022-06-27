8bdd26aac2b608d6708acb1aa64af75ac1f3cd9c
Nazareno Cavazzon <49735945+NazarenoCavazzon@users.noreply.github.com>
2022-06-27 20:35:49 -0300

    all the ListView parameters included

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+## [1.0.2] - ListView Parameters
+
+- Now we pass the padding to the ListViewBuilder instead of using a padding widget
+- All the ListView.builder parameters has been included
+
 ## [1.0.1] - UI Updates
 
 - Padding parameter included
--- lib/src/paginated_list.dart
+++ lib/src/paginated_list.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:visibility_detector/visibility_detector.dart';
 
@@ -31,6 +32,23 @@ class PaginatedList<T> extends StatelessWidget {
     this.scrollDirection = Axis.vertical,
     this.deleteIconAlignment = Alignment.centerRight,
     this.padding = EdgeInsets.zero,
+    this.addAutomaticKeepAlives = true,
+    this.addRepaintBoundaries = true,
+    this.addSemanticIndexes = true,
+    this.controller,
+    this.clipBehavior = Clip.hardEdge,
+    this.dragStartBehavior = DragStartBehavior.start,
+    this.findChildIndexCallback,
+    this.cacheExtent,
+    this.itemExtent,
+    this.primary,
+    this.reverse = false,
+    this.shrinkWrap = false,
+    this.prototypeItem,
+    this.restorationId,
+    this.keyboardDismissBehavior = ScrollViewKeyboardDismissBehavior.manual,
+    this.semanticChildCount,
+    this.listViewKey,
     required this.items,
     required this.isRecentSearch,
     required this.isLastPage,
@@ -69,6 +87,57 @@ class PaginatedList<T> extends StatelessWidget {
   /// Whenever the list is displaying the last page of results.
   final bool isLastPage;
 
+  /// The ListView [addAutomaticKeepAlives] parameter.
+  final bool addAutomaticKeepAlives;
+
+  /// The ListView [addRepaintBoundaries] parameter.
+  final bool addRepaintBoundaries;
+
+  /// The ListView [addSemanticIndexes] parameter.
+  final bool addSemanticIndexes;
+
+  /// The ListView [cacheExtent] parameter.
+  final double? cacheExtent;
+
+  /// The ListView [clipBehavior] parameter.
+  final Clip clipBehavior;
+
+  /// The ListView [controller] parameter.
+  final ScrollController? controller;
+
+  /// The ListView [dragStartBehavior] parameter.
+  final DragStartBehavior dragStartBehavior;
+
+  /// The ListView [findChildIndexCallback] parameter.
+  final int? Function(Key)? findChildIndexCallback;
+
+  /// The ListView [itemExtent] parameter.
+  final double? itemExtent;
+
+  /// The ListView [primary] parameter.
+  final bool? primary;
+
+  /// The ListView [reverse] parameter.
+  final bool reverse;
+
+  /// The ListView [shrinkWrap] parameter.
+  final bool shrinkWrap;
+
+  /// The ListView [prototypeItem] parameter.
+  final Widget? prototypeItem;
+
+  /// The ListView [restorationId] parameter.
+  final String? restorationId;
+
+  /// The ListView [keyboardDismissBehavior] parameter.
+  final ScrollViewKeyboardDismissBehavior keyboardDismissBehavior;
+
+  /// The ListView [semanticChildCount] parameter.
+  final int? semanticChildCount;
+
+  /// The ListView [listViewKey] parameter.
+  final Key? listViewKey;
+
   /// The padding of the [PaginatedList].
   final EdgeInsetsGeometry padding;
 
@@ -78,57 +147,72 @@ class PaginatedList<T> extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final itemCount = items.length + (isRecentSearch || isLastPage ? 0 : 1);
-    return Padding(
+    return ListView.builder(
+      key: listViewKey,
+      scrollDirection: scrollDirection,
+      reverse: reverse,
+      controller: controller,
+      primary: primary,
+      restorationId: restorationId,
+      semanticChildCount: semanticChildCount,
+      shrinkWrap: shrinkWrap,
+      prototypeItem: prototypeItem,
+      keyboardDismissBehavior: keyboardDismissBehavior,
       padding: padding,
-      child: ListView.builder(
-        scrollDirection: scrollDirection,
-        physics: physics,
-        itemCount: itemCount,
-        itemBuilder: (context, index) {
-          return Stack(
-            children: [
-              InkWell(
-                onTap: () => onTap?.call(index),
-                child: Builder(
-                  builder: (context) {
-                    if (index == items.length) {
-                      return VisibilityDetector(
-                        key: const Key('loading-more'),
-                        onVisibilityChanged: (visibility) {
-                          if (visibility.visibleFraction == 1) {
-                            onLoadMore?.call(index);
-                          }
-                        },
-                        child: loadingIndicator,
-                      );
-                    } else {
-                      final item = items[index];
-                      return builder?.call(item, index) ?? const SizedBox();
-                    }
-                  },
-                ),
-              ),
-              if (isRecentSearch)
-                Builder(
-                  builder: (context) {
-                    final item = items[index];
-                    return Align(
-                      alignment: deleteIconAlignment,
-                      child: IconButton(
-                        onPressed: () {
-                          if (item != null) {
-                            onRemove?.call(item, index);
-                          }
-                        },
-                        icon: deleteIcon,
-                      ),
+      itemExtent: itemExtent,
+      findChildIndexCallback: findChildIndexCallback,
+      dragStartBehavior: dragStartBehavior,
+      addAutomaticKeepAlives: addAutomaticKeepAlives,
+      addRepaintBoundaries: addRepaintBoundaries,
+      addSemanticIndexes: addSemanticIndexes,
+      cacheExtent: cacheExtent,
+      clipBehavior: clipBehavior,
+      physics: physics,
+      itemCount: itemCount,
+      itemBuilder: (context, index) {
+        return Stack(
+          children: [
+            InkWell(
+              onTap: () => onTap?.call(index),
+              child: Builder(
+                builder: (context) {
+                  if (index == items.length) {
+                    return VisibilityDetector(
+                      key: const Key('loading-more'),
+                      onVisibilityChanged: (visibility) {
+                        if (visibility.visibleFraction == 1) {
+                          onLoadMore?.call(index);
+                        }
+                      },
+                      child: loadingIndicator,
                     );
-                  },
-                )
-            ],
-          );
-        },
-      ),
+                  } else {
+                    final item = items[index];
+                    return builder?.call(item, index) ?? const SizedBox();
+                  }
+                },
+              ),
+            ),
+            if (isRecentSearch)
+              Builder(
+                builder: (context) {
+                  final item = items[index];
+                  return Align(
+                    alignment: deleteIconAlignment,
+                    child: IconButton(
+                      onPressed: () {
+                        if (item != null) {
+                          onRemove?.call(item, index);
+                        }
+                      },
+                      icon: deleteIcon,
+                    ),
+                  );
+                },
+              )
+          ],
+        );
+      },
     );
   }
 }
